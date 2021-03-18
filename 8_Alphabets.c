6a54c81bbd14933ee2a15e91f7f1f622ffdda262
Aryan-66148 <76278906+Aryan-66148@users.noreply.github.com>
2021-03-18 10:17:39 +0530

    Update 8_Alphabets.c

--- 8_Alphabets.c
+++ 8_Alphabets.c
@@ -4,7 +4,7 @@ int main() {
     int lowercase_vowel, uppercase_vowel;
     printf("Enter an alphabet: ");
     scanf("%c", &c);
-
+            //If user trys any case 
     lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 
     uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
