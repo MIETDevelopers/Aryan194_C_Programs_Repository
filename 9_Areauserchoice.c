a6acf5bfa963d94d0bea6cd9df8a8e0439adcc61
Aryan-66148 <76278906+Aryan-66148@users.noreply.github.com>
2021-03-18 10:20:06 +0530

    Update 9_Areauserchoice.c

--- 9_Areauserchoice.c
+++ 9_Areauserchoice.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
  int main(){
     float radiusofcircle,sideofsquare,lengthofrectangle,widthofrectangle;
-    
+       //Input by user
   scanf("%f %f %f %f", &radiusofcircle, &sideofsquare, &lengthofrectangle ,&widthofrectangle);
-
+         //To sidplay the required 
     printf("Area of circle: %f",((22*radiusofcircle*radiusofcircle)/7));
     printf("\nPerimeter of circle: %f",((2*22*radiusofcircle)/7));
     printf("\nArea of square: %f",(sideofsquare*sideofsquare));
